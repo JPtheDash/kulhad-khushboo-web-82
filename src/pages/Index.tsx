6b28d97ce5d841b190bf9318792f087287529b82
JPtheDash <137363790+JPtheDash@users.noreply.github.com>
2025-06-22 13:08:41 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -145,7 +145,8 @@ const Index = () => {
               </div>
               <div className="relative">
                 <img 
-                  src="Kulhad artist.png" 
+                  src="kulhad-khushboo-web-82
+/Kulhad artist.png" 
                   alt="Traditional pottery making" 
                   className="rounded-2xl shadow-2xl"
                 />
